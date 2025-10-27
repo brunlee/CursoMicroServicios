5d8146dad0389dca7659de1d37ada59b5829e0f0
BFB <73282857+brunlee@users.noreply.github.com>
2025-10-27 13:23:58 +0000

    commit 3

--- Clases/Pizzas.cs
+++ Clases/Pizzas.cs
@@ -1,15 +1,9 @@
 Public class Pizza
 {
-    id,
-  name,
-  description,
-  url,
-  price->suma costo de los ingredientes + 20 % beneficios
-  ingredientes:[{id,name,costo}]
-
     public int Id {get;set;}
     public string Name {get;set;}
     public string Description {get;set;}
     public string Url {get;set;}
-    public decimal Costo {get;set;}
+    public decimal Price {get;set;}
+    public List<Ingrediente>Ingredientes{get;set;}
 }
